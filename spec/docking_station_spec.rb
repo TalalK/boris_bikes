5e66acd7d2c5cf655f232c0a6f4eb9744a3dda22
Talal Khodr <Talal.Khodr@Gmail.com>
2014-07-09 16:03:49 +0100

    Put most tests from station to container

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,34 +2,10 @@ require "docking_station"
 
 describe DockingStation do
 
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
+	let(:station) { DockingStation.new(:capacity => 123) }
 
-	
-	it "Should accept bike" do
-		expect(station.bike_count).to eq(0)
-		station.dock(bike)
-		expect(station.bike_count).to eq(1)
-	end
-
-	it "Should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it is full" do
-		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
-		expect(station).to be_full
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
+	it "Should allow setting default capacity on initialising" do
+		expect(station.capacity).to eq(123)
 	end
 
 end

6c40e44b108977c1f1c45261832633dea82f421e
Talal Khodr <Talal.Khodr@Gmail.com>
2014-07-09 12:02:46 +0100

    fourth commit

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,8 +3,8 @@ require 'bike'
 describe Bike do
 
 	it "should not be broken after we create it" do
-		the_bike = Bike.new
-		expect(the_bike).not_to be_broken
+		bike = Bike.new
+		expect(bike).not_to be_broken
 	
 	end
 
@@ -13,5 +13,13 @@ describe Bike do
 		bike.break!
 		expect(bike).to be_broken
 	end
+
+	it "should be able to get fixed" do
+	bike = Bike.new
+	bike.break!
+	bike.fix!
+	expect(bike).not_to be_broken
+	end	
+
 end
 
